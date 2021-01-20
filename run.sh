5e858154ccf323d8f7c111b92947bac0ad2c662f
seeds-ogawa <ogawa@seeds-std.co.jp>
2021-01-20 17:24:28 +0900

    test

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-amazon-ssm-agent -register -code "${SSM_AGENT_CODE}" -id "${SSM_AGENT_ID}" -region "${AWS_DEFAULT_REGION}" 
+amazon-ssm-agent -register -code "${SSM_AGENT_CODE}" -id "${SSM_AGENT_ID}" -region "ap-northeast-1"
 amazon-ssm-agent
